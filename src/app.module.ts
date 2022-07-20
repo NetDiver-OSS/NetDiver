fb6a67a3eb99d5f8dcfecd22101da58c5cd3a39d
Sébastien DEU <sebastien@deu.sh>
2022-07-20 23:05:29 +0200

    feat(api): use graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import type { ClientOpts } from 'redis';
 import * as redisStore from 'cache-manager-redis-store';
 import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
+    // Core modules
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -12,9 +16,21 @@ import { ConfigModule } from '@nestjs/config';
       store: redisStore,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: process.env.NODE_ENV !== 'production',
+      playground: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: true,
+      cors: true,
+      autoTransformHttpErrors: true,
+      subscriptions: {
+        'graphql-ws': { path: '/graphql' },
+        'subscriptions-transport-ws': true,
+      },
+      buildSchemaOptions: {
+        dateScalarMode: 'timestamp',
+      },
     }),
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
