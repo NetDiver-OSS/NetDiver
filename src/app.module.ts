96b1bfcd81a69647e578bb9af45ca8d3f1b1c9f8
Vincent Fricou <vincent@fricouv.eu>
2022-07-20 23:03:19 +0200

    add(module): configuration for redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import type { ClientOpts } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.register<ClientOpts>({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
   ],
   controllers: [],
