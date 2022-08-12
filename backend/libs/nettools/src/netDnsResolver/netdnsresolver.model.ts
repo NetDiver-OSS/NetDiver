684a595cff2119627a0957accdc28be99752f32d
Vincent Fricou <vincent@fricouv.eu>
2022-08-12 18:27:31 +0200

    feat(nettools/dns_resolver): DNS resolver router

---
+++ backend/libs/nettools/src/netDnsResolver/netdnsresolver.model.ts
@@ -0,0 +1,47 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  CaaType,
+  MxType,
+  NaptrType,
+  SoaType,
+  SrvType,
+} from './netdnsresolver.type';
+
+@ObjectType()
+export class NetDnsResolver {
+  @Field(() => [String], { nullable: true })
+  a?: string[];
+
+  @Field(() => [String], { nullable: true })
+  aaaa?: string[];
+
+  // @Field(,{ nullable: true })
+  // any: AnyRecord[] | null;
+  //
+  @Field(() => [CaaType], { nullable: true })
+  caa?: CaaType[];
+
+  @Field(() => [String], { nullable: true })
+  cname?: string[];
+
+  @Field(() => [MxType], { nullable: true })
+  mx?: MxType[];
+
+  @Field(() => [NaptrType], { nullable: true })
+  naptr?: NaptrType[];
+
+  @Field(() => [String], { nullable: true })
+  ns?: string[];
+
+  @Field(() => [String], { nullable: true })
+  ptr?: string[];
+
+  @Field(() => SoaType, { nullable: true })
+  soa?: SoaType;
+
+  @Field(() => [SrvType], { nullable: true })
+  srv?: SrvType[];
+
+  @Field(() => [[String]], { nullable: true })
+  txt?: string[][];
+}
--- backend/libs/nettools/src/nettools.module.ts
+++ backend/libs/nettools/src/nettools.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { NettoolsService } from './nettools.service';
 import { MacAddressModule } from '@netdiver/nettools/macAddress/macaddress.module';
 import { NetCalculatorModule } from '@netdiver/nettools/netCalculator/netcalculator.module';
-import { DatabaseModule } from "../../../src/database/database.module";
+import { DatabaseModule } from '../../../src/database/database.module';
+import { NetDnsResolverModule } from '@netdiver/nettools/netDnsResolver/netdnsresolver.module';
 
 @Module({
   providers: [NettoolsService],
-  exports: [NettoolsService, MacAddressModule, NetCalculatorModule],
-  imports: [DatabaseModule, MacAddressModule, NetCalculatorModule],
+  exports: [
+    NettoolsService,
+    MacAddressModule,
+    NetCalculatorModule,
+    NetDnsResolverModule,
+  ],
+  imports: [
+    DatabaseModule,
+    MacAddressModule,
+    NetCalculatorModule,
+    NetDnsResolverModule,
+  ],
 })
 export class NettoolsModule {}
