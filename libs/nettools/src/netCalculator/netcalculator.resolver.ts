4bfab7174cce77b4bf618421c6b5e627eb547ac3
Vincent Fricou <vincent@fricouv.eu>
2022-08-18 21:18:23 +0200

    refactor(nettools/calculator): reduce code complexity

--- libs/nettools/src/netCalculator/netcalculator.resolver.ts
+++ libs/nettools/src/netCalculator/netcalculator.resolver.ts
@@ -6,277 +6,80 @@ import {
   IPv4CidrRange,
   IPv6CidrRange,
   IPv6Prefix,
-  expandIPv6Number,
 } from 'ip-num';
 
 @Resolver(() => NetCalculator)
 export class NetCalculatorResolver {
+  private validateAddress(network) {
+    if (network.split('.').length < 1 || network.split(':').length < 1) {
+      return;
+    }
+  }
+
+  private getRangeData(ip: IPv4CidrRange | IPv6CidrRange): NetCalculator {
+    const iprange = new NetCalculator();
+
+    iprange.bitmask = ip.getPrefix().toString();
+    iprange.mask = ip.getPrefix().toMask().toString();
+    iprange.network = ip.getFirst().toString();
+    iprange.broadcast = ip.getLast().toString();
+    iprange.size = ip.getSize().toString();
+    iprange.range = ip.toRangeString();
+    iprange.first = ip.getFirst().nextIPNumber().toString();
+    iprange.last = ip.getLast().previousIPNumber().toString();
+
+    return iprange;
+  }
+
   @Query(() => NetCalculator)
   async getNetRange(@Args('network') network: string): Promise<NetCalculator> {
-    const ipv4FormatValidator: string[] = network.split('.');
-    const ipv6FormatValidator: string[] = network.split(':');
-    const netRangeInfos = new NetCalculator();
-
-    if (ipv4FormatValidator.length > 1) {
-      if (Validator.isValidIPv4CidrRange(network)[0]) {
-        netRangeInfos.bitmask = IPv4CidrRange.fromCidr(network)
-          .getPrefix()
-          .toString();
-        netRangeInfos.mask = IPv4CidrRange.fromCidr(network)
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.network = IPv4CidrRange.fromCidr(network)
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv4CidrRange.fromCidr(network)
-          .getLast()
-          .toString();
-        netRangeInfos.size = IPv4CidrRange.fromCidr(network)
-          .getSize()
-          .toString();
-        netRangeInfos.range = IPv4CidrRange.fromCidr(network).toRangeString();
-        netRangeInfos.first = IPv4CidrRange.fromCidr(network)
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv4CidrRange.fromCidr(network)
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
-    } else if (ipv6FormatValidator.length > 1) {
-      if (Validator.isValidIPv6CidrRange(network)[0]) {
-        netRangeInfos.bitmask = IPv6CidrRange.fromCidr(network)
-          .getPrefix()
-          .toString();
-        netRangeInfos.mask = IPv6CidrRange.fromCidr(network)
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.network = IPv6CidrRange.fromCidr(network)
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv6CidrRange.fromCidr(network)
-          .getLast()
-          .toString();
-        netRangeInfos.size = IPv6CidrRange.fromCidr(network)
-          .getSize()
-          .toString();
-        netRangeInfos.range = IPv6CidrRange.fromCidr(network).toRangeString();
-        netRangeInfos.first = IPv6CidrRange.fromCidr(network)
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv6CidrRange.fromCidr(network)
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
+    this.validateAddress(network);
+    if (Validator.isValidIPv4CidrRange(network)[0]) {
+      return this.getRangeData(IPv4CidrRange.fromCidr(network));
+    } else if (Validator.isValidIPv6CidrRange(network)[0]) {
+      return this.getRangeData(IPv6CidrRange.fromCidr(network));
     }
-    return netRangeInfos;
   }
 
   @Query(() => NetCalculator)
   async getNetRangePrevious(
     @Args('network') network: string,
   ): Promise<NetCalculator> {
-    const ipv4FormatValidator: string[] = network.split('.');
-    const ipv6FormatValidator: string[] = network.split(':');
-    const netRangeInfos = new NetCalculator();
+    this.validateAddress(network);
 
-    if (ipv4FormatValidator.length > 1) {
-      if (Validator.isValidIPv4CidrRange(network)[0]) {
-        netRangeInfos.size = IPv4CidrRange.fromCidr(
+    if (Validator.isValidIPv4CidrRange(network)[0]) {
+      return this.getRangeData(
+        IPv4CidrRange.fromCidr(
           IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getSize()
-          .toString();
-        netRangeInfos.mask = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.bitmask = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getPrefix()
-          .toString();
-        netRangeInfos.network = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getLast()
-          .toString();
-        netRangeInfos.range = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        ).toRangeString();
-        netRangeInfos.first = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
-    } else if (ipv6FormatValidator.length > 1) {
-      const netSplit: string[] = network.split('/');
-      if (Validator.isValidIPv6CidrRange(network)) {
-        netRangeInfos.size = IPv6CidrRange.fromCidr(
+        ),
+      );
+    } else if (Validator.isValidIPv6CidrRange(network)[0]) {
+      return this.getRangeData(
+        IPv6CidrRange.fromCidr(
           IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getSize()
-          .toString();
-        netRangeInfos.mask = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.bitmask = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getPrefix()
-          .toString();
-        netRangeInfos.network = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getLast()
-          .toString();
-        netRangeInfos.range = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        ).toRangeString();
-        netRangeInfos.first = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).previousRange().toCidrString(),
-        )
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
+        ),
+      );
     }
-    return netRangeInfos;
   }
 
   @Query(() => NetCalculator)
   async getNetRangeNext(
     @Args('network') network: string,
   ): Promise<NetCalculator> {
-    const ipv4FormatValidator: string[] = network.split('.');
-    const ipv6FormatValidator: string[] = network.split(':');
-    const netRangeInfos = new NetCalculator();
-
-    if (ipv4FormatValidator.length > 1) {
-      if (Validator.isValidIPv4CidrRange(network)[0]) {
-        netRangeInfos.size = IPv4CidrRange.fromCidr(
+    this.validateAddress(network);
+    if (Validator.isValidIPv4CidrRange(network)[0]) {
+      return this.getRangeData(
+        IPv4CidrRange.fromCidr(
           IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getSize()
-          .toString();
-        netRangeInfos.mask = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.bitmask = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getPrefix()
-          .toString();
-        netRangeInfos.network = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getLast()
-          .toString();
-        netRangeInfos.range = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        ).toRangeString();
-        netRangeInfos.first = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv4CidrRange.fromCidr(
-          IPv4CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
-    } else if (ipv6FormatValidator.length > 1) {
-      const netSplit: string[] = network.split('/');
-      if (Validator.isValidIPv6CidrRange(network)) {
-        netRangeInfos.size = IPv6CidrRange.fromCidr(
+        ),
+      );
+    } else if (Validator.isValidIPv6CidrRange(network)[0]) {
+      return this.getRangeData(
+        IPv6CidrRange.fromCidr(
           IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getSize()
-          .toString();
-        netRangeInfos.mask = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getPrefix()
-          .toMask()
-          .toString();
-        netRangeInfos.bitmask = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getPrefix()
-          .toString();
-        netRangeInfos.network = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getFirst()
-          .toString();
-        netRangeInfos.broadcast = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getLast()
-          .toString();
-        netRangeInfos.range = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        ).toRangeString();
-        netRangeInfos.first = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getFirst()
-          .nextIPNumber()
-          .toString();
-        netRangeInfos.last = IPv6CidrRange.fromCidr(
-          IPv6CidrRange.fromCidr(network).nextRange().toCidrString(),
-        )
-          .getLast()
-          .previousIPNumber()
-          .toString();
-      }
+        ),
+      );
     }
-    return netRangeInfos;
   }
 
   @Query(() => NetCalculator)
@@ -285,25 +88,21 @@ export class NetCalculatorResolver {
     @Args('into') into: string,
   ): Promise<NetCalculator> {
     const netRanges = new Array<string>();
-    const ipv4FormatValidator: string[] = network.split('.');
-    const ipv6FormatValidator: string[] = network.split(':');
+    this.validateAddress(network);
 
-    if (ipv4FormatValidator.length > 1) {
-      if (Validator.isValidIPv4CidrRange(network)[0]) {
-        IPv4CidrRange.fromCidr(network)
-          .splitInto(
-            new IPv4Prefix(
-              IPv4CidrRange.fromCidr(
-                ''.concat('0.0.0.0/', into),
-              ).getPrefix().value,
-            ),
-          )
-          .forEach((element: IPv4CidrRange) => {
-            netRanges.push(element.toCidrString());
-          });
-      }
-    } else if (ipv6FormatValidator.length > 1) {
-      const netSplit: string[] = network.split('/');
+    if (Validator.isValidIPv4CidrRange(network)[0]) {
+      IPv4CidrRange.fromCidr(network)
+        .splitInto(
+          new IPv4Prefix(
+            IPv4CidrRange.fromCidr(
+              ''.concat('0.0.0.0/', into),
+            ).getPrefix().value,
+          ),
+        )
+        .forEach((element: IPv4CidrRange) => {
+          netRanges.push(element.toCidrString());
+        });
+    } else if (Validator.isValidIPv6CidrRange(network)[0]) {
       IPv6CidrRange.fromCidr(network)
         .splitInto(
           new IPv6Prefix(
