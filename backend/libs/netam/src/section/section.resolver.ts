c92c3aa9aa5bfe08af125aadfcd336716a70f095
Vincent Fricou <vincent@fricouv.eu>
2022-09-02 09:13:14 +0200

    feat(netam/section_resolver): add multiples routers
    - getSectionId
    - getSectionName
    - getSectionNetwork
    - getSectionScanType
    - getSectionByVlanName

--- backend/libs/netam/src/section/section.resolver.ts
+++ backend/libs/netam/src/section/section.resolver.ts
@@ -1,6 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { Section } from '@netdiver/netam/section/section.model';
-import { PrismaService } from "../../../../src/database/prisma.service";
+import { PrismaService } from '../../../../src/database/prisma.service';
 
 @Resolver(() => Section)
 export class SectionResolver {
@@ -9,5 +9,72 @@ export class SectionResolver {
   @Query(() => [Section])
   async getSections(): Promise<Section[]> {
     return this.prismaService.sections.findMany({ include: { vlan: true } });
+  }
+
+  @Query(() => Section)
+  async getSectionId(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Section> {
+    return this.prismaService.sections.findFirst({
+      orderBy: [{ id: 'asc' }],
+      where: { id: id },
+      include: { vlan: true },
+    });
+  }
+
+  @Query(() => [Section])
+  async getSectionName(@Args('name') name: string): Promise<Section[]> {
+    return this.prismaService.sections.findMany({
+      orderBy: [{ name: 'asc' }],
+      where: {
+        name: {
+          contains: name,
+        },
+      },
+      include: { vlan: true },
+    });
+  }
+
+  @Query(() => [Section])
+  async getSectionNetwork(
+    @Args('network') network: string,
+  ): Promise<Section[]> {
+    return this.prismaService.sections.findMany({
+      orderBy: [{ name: 'asc' }],
+      where: {
+        network: {
+          contains: network,
+        },
+      },
+      include: { vlan: true },
+    });
+  }
+
+  @Query(() => [Section])
+  async getSectionScanType(
+    @Args('scantype') scantype: string,
+  ): Promise<Section[]> {
+    return this.prismaService.sections.findMany({
+      orderBy: [{ name: 'asc' }],
+      where: {
+        scantype: {
+          contains: scantype,
+        },
+      },
+      include: { vlan: true },
+    });
+  }
+
+  @Query(() => [Section])
+  async getSectionByVlanName(@Args('vlan') vlan: string): Promise<Section[]> {
+    return this.prismaService.sections.findMany({
+      orderBy: [{ name: 'asc' }],
+      where: {
+        vlan: {
+          name: vlan,
+        },
+      },
+      include: { vlan: true },
+    });
   }
 }
