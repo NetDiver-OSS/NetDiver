3d1a16c2db524656c01b341cd2e4264669360f5a
Vincent Fricou <vincent@fricouv.eu>
2022-09-05 22:27:48 +0200

    feat(netam/usages_resolver): usage resolver

--- backend/libs/netam/src/usage/usage.resolver.ts
+++ backend/libs/netam/src/usage/usage.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from '../../../../src/database/prisma.service';
 import { Usage } from '@netdiver/netam/usage/usage.model';
+import { UsageState } from './usage.type';
 
 @Resolver(() => Usage)
 export class UsageResolver {
@@ -9,5 +10,41 @@ export class UsageResolver {
   @Query(() => [Usage])
   async getUsages(): Promise<Usage[]> {
     return this.prismaService.usages.findMany();
+  }
+
+  @Query(() => Usage)
+  async getUsageId(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Usage> {
+    return this.prismaService.usages.findFirst({
+      orderBy: [{ id: 'asc' }],
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  @Query(() => [Usage])
+  async getUsageFqdn(@Args('fqdn') fqdn: string): Promise<Usage[]> {
+    return this.prismaService.usages.findMany({
+      orderBy: [{ fqdn: 'asc' }],
+      where: {
+        fqdn: {
+          contains: fqdn,
+        },
+      },
+    });
+  }
+
+  @Query(() => [Usage])
+  async getUsageStatus(@Args('status') status: string): Promise<Usage[]> {
+    return this.prismaService.usages.findMany({
+      orderBy: [{ status: 'asc' }],
+      where: {
+        status: {
+          equals: UsageState[status],
+        },
+      },
+    });
   }
 }
