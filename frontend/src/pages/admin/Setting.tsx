089fba25e59e00b1cb6d4740a77837cd222c44a4
Sébastien DEU <sebastien@deu.sh>
2022-09-05 22:03:43 +0200

    feat(frontend): add eslint configuration

--- frontend/src/pages/admin/Setting.tsx
+++ frontend/src/pages/admin/Setting.tsx
@@ -5,7 +5,12 @@ export const Setting: FunctionComponent = () => {
     <>
       <h1>Settings</h1>
       <p>
-        Ready are you? What know you of ready? For eight hundred years have I trained Jedi. My own counsel will I keep on who is to be trained. A Jedi must have the deepest commitment, the most serious mind. This one a long time have I watched. All his life has he looked away - to the future, to the horizon. Never his mind on where he was. Hmm? What he was doing. Hmph. Adventure. Heh. Excitement. Heh. A Jedi craves not these things. You are reckless.
+        Ready are you? What know you of ready? For eight hundred years have I trained Jedi.
+        My own counsel will I keep on who is to be trained.
+        A Jedi must have the deepest commitment, the most serious mind.
+        This one a long time have I watched. All his life has he looked away - to the future, to the horizon.
+        Never his mind on where he was. Hmm? What he was doing. Hmph. Adventure. Heh. Excitement. Heh.
+        A Jedi craves not these things. You are reckless.
       </p>
     </>
   )
--- frontend/src/pages/admin/Worker.tsx
+++ frontend/src/pages/admin/Worker.tsx
@@ -5,7 +5,12 @@ export const Worker: FunctionComponent = () => {
     <>
       <h1>Worker</h1>
       <p>
-        Size matters not. Look at me. Judge me by my size, do you? Hmm? Hmm. And well you should not. For my ally is the Force, and a powerful ally it is. Life creates it, makes it grow. Its energy surrounds us and binds us. Luminous beings are we, not this crude matter. You must feel the Force around you; here, between you, me, the tree, the rock, everywhere, yes. Even between the land and the ship.
+        Size matters not. Look at me. Judge me by my size, do you? Hmm? Hmm.
+        And well you should not. For my ally is the Force, and a powerful ally it is.
+        Life creates it, makes it grow. Its energy surrounds us and binds us.
+        Luminous beings are we, not this crude matter.
+        You must feel the Force around you; here, between you, me, the tree, the rock, everywhere, yes.
+        Even between the land and the ship.
       </p>
     </>
   )
