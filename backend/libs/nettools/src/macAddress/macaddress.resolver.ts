368bdfc4987c905dfcfe9037882dd9d4996ac5ee
Vincent Fricou <vincent@fricouv.eu>
2022-11-04 22:30:10 +0100

    add(netdiver/macaddress): syncOuiDatabase function

--- backend/libs/nettools/src/macAddress/macaddress.resolver.ts
+++ backend/libs/nettools/src/macAddress/macaddress.resolver.ts
@@ -1,6 +1,8 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { MacAddress } from './macaddress.model';
+import { MacAddress, OuiSync } from './macaddress.model';
 import { PrismaService } from '../../../../src/database/prisma.service';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Resolver(() => MacAddress)
 export class MacAddressResolver {
@@ -58,5 +60,46 @@ export class MacAddressResolver {
         },
       },
     });
+  }
+
+  @Query(() => OuiSync)
+  async syncOuiDatabase(): Promise<OuiSync> {
+    const httpService = new HttpService();
+    const { data: rawDatas } = await firstValueFrom(
+      httpService.get('https://standards-oui.ieee.org/oui/oui.txt'),
+    );
+    type ouiType = {
+      mac: string;
+      vendor: string;
+    };
+    const ouiDatas: ouiType[] = [];
+    rawDatas.split('\n').forEach((line) => {
+      if (line.match('(hex)')) {
+        const lineData = line
+          .match('(hex)')
+          .input.replace('\t\t', '')
+          .replace('\r', '')
+          .split('(hex)');
+        ouiDatas.push({
+          mac: lineData[0].trim(),
+          vendor: lineData[1]?.trim(),
+        });
+      }
+    });
+    try {
+      await this.prismaService.$transaction([
+        this.prismaService.$executeRawUnsafe(
+          'TRUNCATE TABLE nettools_macaddresses',
+        ),
+        this.prismaService.macAddresses.createMany({ data: ouiDatas }),
+      ]);
+      return {
+        result: true,
+      };
+    } catch (error) {
+      return {
+        result: false,
+      };
+    }
   }
 }
