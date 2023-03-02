d4855f0b02e52653458b717e03f57b2d9d4e8ccb
Vincent Fricou <vincent@fricouv.eu>
2023-03-02 15:47:09 +0100

    feat(back/netam/usages): updateUsage graphql route

--- backend/libs/netam/src/usage/usage.resolver.ts
+++ backend/libs/netam/src/usage/usage.resolver.ts
@@ -74,4 +74,49 @@ export class UsageResolver {
     });
     return this.getUsageIdentifier(identifier);
   }
+  @Mutation(() => Usage)
+  async updateUsage(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('ipUsed', { nullable: true }) ipUsed: string,
+    @Args('description', { nullable: true }) description: string,
+    @Args('fqdn', { nullable: true }) fqdn: string,
+    @Args('status', { nullable: true }) status: string,
+    @Args('sectionId', { type: () => Int, nullable: true }) sectionId: number,
+  ): Promise<Usage> {
+    const usageData: Partial<UsageInputCreation> = {};
+    if (sectionId || ipUsed) {
+      const usageCurrentInfo = await this.prismaService.usages.findUnique({
+        select: {
+          ipUsed: true,
+          sectionId: true,
+        },
+        where: {
+          id: id,
+        },
+      });
+      if (sectionId) {
+        if (ipUsed) {
+          usageData.identifier = ipUsed + '_' + sectionId;
+        } else {
+          usageData.sectionId = sectionId;
+          usageData.identifier = usageCurrentInfo.ipUsed + '_' + sectionId;
+        }
+      } else if (ipUsed) {
+        usageData.identifier = ipUsed + '_' + usageCurrentInfo.sectionId;
+        usageData.ipUsed = ipUsed;
+      }
+    }
+
+    if (description) usageData.description = description;
+    if (fqdn) usageData.fqdn = fqdn;
+    // if (status) usageData.status = status; //TODO : Add state management -> Enum
+
+    await this.prismaService.usages.update({
+      where: {
+        id: id,
+      },
+      data: usageData,
+    });
+    return this.getUsageId(id);
+  }
 }
